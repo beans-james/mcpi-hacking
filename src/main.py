5743db7babd4378c1297bc6aece2a532c59d1cb5
beans-james <27931@sgs.uk.net>
2026-05-22 12:51:47 +0100

    Add option to run Instant Mega Spawner script

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ def menu():
     print("0 -> Exit")
     print("1 -> Run Diamond Body Script (diabody.py)")
     print("2 -> Run Midas Touch Script (midas.py)")
+    print("3 -> Run Instant Mega Spawner (spawner.py)")
     
     choice = input("Select an option (0-2): ").strip()
 
@@ -17,6 +18,9 @@ def menu():
     elif choice == "2":
         print("\nLaunching Midas Touch...")
         import midas
+    elif choice == "3":
+        print("\nLaunching Mega Spawner...")
+        import spawner
     else:
         print("Invalid choice! Try again.")
         menu()
