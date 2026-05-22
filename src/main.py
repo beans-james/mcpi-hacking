25a50770873748973d67008571be5b4115843306
beans-james <27931@sgs.uk.net>
2026-05-22 12:33:53 +0100

    Refactor block placement in Minecraft script
    Removed chat message and diamond block spawn above player.

--- src/main.py
+++ src/main.py
@@ -5,9 +5,6 @@ mc = init()
 if mc:
     # Get the player's current position
     x, y, z = mc.player.getTilePos()
-    
-    # Post a message to the in-game chat
-    mc.postToChat("Hello from Python!")
-    
-    # Spawn a diamond block right above your head
-    mc.setBlock(x, y + 2, z, 57)
+
+    mc.setBlock(x, y - 1, z, 57)
+    mc.setBlock(x, y + 1, z, 57)
